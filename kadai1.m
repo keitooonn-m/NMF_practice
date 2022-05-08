0fa134c1b7d7e321fbaef5a24cba5f22df5528e8
K-Karato <86594359+pipikouki@users.noreply.github.com>
2022-05-08 14:39:10 +0900

    kadai1,kadai2完成、kadai3に向けて論文読みます

--- kadai1.m
+++ kadai1.m
@@ -1,7 +1,17 @@
+clc;clear;
+
+filename = "./input/03_03.txt"; %分解したいデータ
+rep = 100;              %繰り返し回数
+K_const = 2;            %分解の定数
+
+%W,Hの初期値はmain.mで乱数に設定されている
+kadai1_main(filename,rep,K_const);
+
+%--------------------ここからmain関数--------------------------
 function kadai1_main(filename,rep,K_const)
 
 fprintf(filename);
-input = importdata("./input/" + filename);
+input = importdata(filename);
 
 distance = zeros(rep,3);
 
--- kadai1_call_main.m
+++
@@ -1,8 +0,0 @@
-clc;clear;
-
-filename = "03_03.txt"; %分解したいデータ
-rep = 100;              %繰り返し回数
-K_const = 2;            %分解の定数
-
-%W,Hの初期値はmain.mで乱数に設定されている
-kadai1_main(filename,rep,K_const);
