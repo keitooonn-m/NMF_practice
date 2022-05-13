70032025b61438a4951eba1372151db690aa205c
K-Karato <86594359+pipikouki@users.noreply.github.com>
2022-05-13 23:15:49 +0900

    ミスってた修正

--- kadai3.m
+++ kadai3.m
@@ -9,26 +9,20 @@ teacher2_filename = "./scaleData/gpo_hr_scale.wav";
 rep = 512;
 K_const = 32;
 
-windowname = "Blackman";
-F = DGTtool('windowName',windowname);
+F = DGTtool(windowname = "Blackman");
 
 data_vec = audioread(data_filename);
 [teacher1_vec,fs] = audioread(teacher1_filename);
 [teacher2_vec, ~] = audioread(teacher2_filename);
 
-Y = abs(F(data_vec));
-data_theta_spec = angle(F(data_vec));
 
-D1 = abs(F(teacher1_vec));
-D2 = abs(F(teacher2_vec));
-
-%基底ベクトルとる
-[W1,~,~] =NMF_KL_auto(D1,rep,K_const);
-[W2,~,~] =NMF_KL_auto(D2,rep,K_const);
+%教師音をSTFT、絶対値とって基底ベクトル作成する
+[W1,~,~] =NMF_KL_auto(abs(F(teacher1_vec)),rep,K_const);
+[W2,~,~] =NMF_KL_auto(abs(F(teacher2_vec)),rep,K_const);
 
 %分解する
-[G1,G2,distance] = NMF_KL_bunri(Y,W1,W2,rep);
-plot(distance);
+[G1,G2,distance] = NMF_KL_bunri(abs(F(data_vec)),W1,W2,rep);
+plot(distance); %分解後の誤差関数減少の確認はこちら
 
 %位相追加
 output1 = W1*G1  ./ (W1*G1 + W2*G2) .* F(data_vec);
@@ -42,15 +36,12 @@ result_data2 = F.pinv(output2);
 audiowrite("./output/kadai3_output1.wav",result_data1,fs);
 audiowrite("./output/kadai3_output2.wav",result_data2,fs);
 
-%SDR確認する
+%SDR確認するために真のデータ読み込み
 addpath("./bss_eval");
-true_data1 = audioread("./scaleData/gpo_cl_scale.wav");
-true_data2 = audioread("./scaleData/gpo_hr_scale.wav");
+true_data1 = audioread("./songKitamura/MGS/melody2/mgs_cl.wav");
+true_data2 = audioread("./songKitamura/MGS/melody1/mgs_hr.wav");
 true_data = [true_data1,true_data2];
-
-%なんか知らんけど0ぱってぃんぐしたデータのほうが短い
 result_data = [result_data1,result_data2];
-[length,~] = size(result_data);
 
-bss_eval_sources(result_data(:,1)',true_data(1:length,1)')
-bss_eval_sources(result_data(:,2)',true_data(1:length,2)')
+%0パッティングした分だけ結果を短くしないといけない
+bss_eval_sources(result_data(1:size(true_data,1),:)',true_data')
